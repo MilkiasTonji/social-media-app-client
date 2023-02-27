c1c1455eacc2f22b9470a107cab8c183d00ba84c
abebe.kayimo@crowdbotics.com <tongisttonji@gmail.com>
2023-02-27 23:06:04 +0300

    Latest update on Feb27 2023

--- src/scenes/loginPage/Form.jsx
+++ src/scenes/loginPage/Form.jsx
@@ -13,3 +13,93 @@ import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import { Formik } from "formik";
 import * as yup from 'yup';
 import { useNavigate } from "react-router-dom";
+
+import { useDispatch } from "react-redux";
+import { setLogin } from "state";
+import Dropzone from "react-dropzone";
+import FlexBetween from "components/FlexBetween";
+
+
+
+// create Schema 
+const registerSchema = yup.object().shape({
+    firstName: yup.string().required('required'),
+    lastName: yup.string().required('required'),
+    email: yup.string().email('Invalid email').required('required'),
+    password: yup.string().required('required'),
+    location: yup.string().required('required'),
+    occupation: yup.string().required('required'),
+    picture: yup.string().required('required'),
+})
+
+const loginSchema = yup.object().shape({
+    email: yup.string().email('Invalid email').required('required'),
+    password: yup.string().required('required')
+})
+
+const initialValuesRegister = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    location: "",
+    occupation: "",
+    picture: ""
+}
+
+const initialValuesLogin = {
+    firstName: "",
+    password: ""
+}
+
+
+const Form = () => {
+    const [pageType, setPageType] = useState("login")
+    const {palette} = useTheme();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const isNonMobile = useMediaQuery("(min-width:600px");
+    const isLogin = pageType === "login"
+    const isRegister = pageType === "register";
+
+    const handleFormSubmit = async(values, onSubmitProps)=> {
+
+    }
+
+    return <Formik
+        onSubmit={handleFormSubmit}
+        initialValues={isLogin ? initialValuesLogin: initialValuesRegister}
+        validationSchema={isLogin ? loginSchema: registerSchema}
+    >
+     {({
+        values,
+        errors,
+        touched,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+        setFieldValue,
+        resetForm
+     }) => {
+        <form onSubmit={handleSubmit}>
+            <Box
+            display="grid"
+            gap="30px"
+            gridTemplateColumns="repeat(4, minmax(0, 1fr))"
+            sx={{
+                "& > div": {gridColumn: isNonMobile ? undefined: "span 4"}
+            }}
+            >
+
+            </Box>
+        </form>
+     }}   
+
+    </Formik>
+
+}
+
+
+
+
+export default Form
