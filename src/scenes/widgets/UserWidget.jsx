60308a375d02396632c76bf36fc7411f77a85847
abebe.kayimo@crowdbotics.com <tongisttonji@gmail.com>
2023-03-02 15:07:09 +0300

    Latest update on March2 2023b

--- src/scenes/widgets/UserWidget.jsx
+++ src/scenes/widgets/UserWidget.jsx
@@ -1,5 +1,8 @@
-import { ManageAccountsOutlined } from "@mui/icons-material";
-import { Typography, useTheme } from "@mui/material";
+import {
+  LocationOnOutlined,
+  ManageAccountsOutlined,
+} from "@mui/icons-material";
+import { Divider, Typography, useTheme } from "@mui/material";
 import FlexBetween from "components/FlexBetween";
 import UserImage from "components/UserImage";
 import WidgetWrapper from "components/WidgetWrapper";
@@ -7,76 +10,88 @@ import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
+const UserWidget = ({ userId, picturePth }) => {
+  const [user, setUser] = useState(null);
+  const { palette } = useTheme();
+  const navigate = useNavigate();
+  const token = useSelector((state) => state.totken);
+  const dark = palette.neutral.dark;
+  const medium = palette.neutral.medium;
+  const main = palette.neutral.main;
 
+  const getUser = async () => {
+    const response = await fetch(`http://localhost:3001/${userId}`, {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
 
-
-const UserWidget = ({userId, picturePth}) => {
-    const [user, setUser] = useState(null);
-    const {palette} = useTheme();
-    const navigate = useNavigate();
-    const token = useSelector((state) => state.totken);
-    const dark = palette.neutral.dark;
-    const medium = palette.neutral.medium;
-    const main = palette.neutral.main;
-
-
-    const getUser = async () => {
-        const response = await fetch(`http://localhost:3001/${userId}`,{
-            method: "GET",
-            headers: {Authorization: `Bearer ${token}`}
-        });
-
-        const data = await response.json();
-        if(data){
-            setUser(data)
-        }else{
-            setUser(null)
-        }
+    const data = await response.json();
+    if (data) {
+      setUser(data);
+    } else {
+      setUser(null);
     }
+  };
 
-    useEffect(() => {
-        getUser()
-    }, []) // eslint-disable-line react-hooks/exhaustive-deps
+  useEffect(() => {
+    getUser();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
+  if (!user) {
+    return null;
+  }
 
-    if(!user){
-        return null
-    }
+  const {
+    firstName,
+    lastName,
+    location,
+    ocuppation,
+    viewedProfile,
+    impressions,
+    friends,
+    picturePth,
+  } = user;
 
-    const {firstName, lastName, location, ocuppation, viewedProfile, impressions, friends, picturePth } = user
-
-    return (
-        <WidgetWrapper>
-            {/* First Row */}
-            <FlexBetween gap="0.5rem" pb="1.1rem" onClick={()=> navigate(`/profile/${userId}`)}>
-                <FlexBetween gap="1rem">
-                    <UserImage image={picturePth} />
-                    <Box>
-                        <Typography 
-                            variant="h4" 
-                            color={dark} 
-                            fontWeight="500"
-                            sx={{
-                                "&:hover": {
-                                    color: palette.primary.light,
-                                    cursor: 'pointer'
-                                }
-                            }}
-                        >
-                            {firstName} {lastName}
-                        </Typography>
-                        <Typography color={medium}>
-                            {friends.length} Friends                            
-                        </Typography>
-                    </Box>
-                    <ManageAccountsOutlined />
-                </FlexBetween>
-            </FlexBetween>
-        </WidgetWrapper>
-    )
-
-}
-
-
+  return (
+    <WidgetWrapper>
+      {/* First Row */}
+      <FlexBetween
+        gap="0.5rem"
+        pb="1.1rem"
+        onClick={() => navigate(`/profile/${userId}`)}
+      >
+        <FlexBetween gap="1rem">
+          <UserImage image={picturePth} />
+          <Box>
+            <Typography
+              variant="h4"
+              color={dark}
+              fontWeight="500"
+              sx={{
+                "&:hover": {
+                  color: palette.primary.light,
+                  cursor: "pointer",
+                },
+              }}
+            >
+              {firstName} {lastName}
+            </Typography>
+            <Typography color={medium}>{friends.length} Friends</Typography>
+          </Box>
+          <ManageAccountsOutlined />
+        </FlexBetween>
+        {/* DIVIDER */}
+        <Divider />
+        {/* SECOND ROW */}
+        <Box p="1rem 0">
+          <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+            <LocationOnOutlined fontSize="large" sx={{ color: main }} />
+            <Typography color={medium}>{location}</Typography>
+          </Box>
+        </Box>
+      </FlexBetween>
+    </WidgetWrapper>
+  );
+};
 
 export default UserWidget;
