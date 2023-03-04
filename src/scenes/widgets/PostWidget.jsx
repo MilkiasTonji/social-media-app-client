9be022448f1d4c8201e92eb582861c501a6d8cec
abebe.kayimo@crowdbotics.com <tongisttonji@gmail.com>
2023-03-04 16:14:43 +0300

    Latest update on March4 2023

--- src/scenes/widgets/PostWidget.jsx
+++ src/scenes/widgets/PostWidget.jsx
@@ -1,13 +1,40 @@
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state";
 
+const PostsWidget = ({ userId, isProfile = false }) => {
+  const dispatch = useDispatch();
+  const posts = useSelector((state) => state.posts);
+  const token = useSelector((state) => state.token);
 
+  const getPosts = async () => {
+    const response = await fetch("http://localhost:3001/posts", {
+      method: "GET",
+      headers: { Authorization: `Bearer ${token}` },
+    });
+    const data = await response.json();
+    dispatch(setPosts({ posts: data }));
+  };
 
-const PostsWidget = ({userId, isProfile = false})=> {
-    const dispatch = useDispatch()
-    const posts = useSelector((state)=> state.posts)
-    const token = useSelector((state)=> state.token)
-    
-}
+  const getUserPosts = async () => {
+    const response = await fetch(
+      `http://localhost:3001/posts/${userId}/posts`,
+      {
+        method: "GET",
+        headers: { Authorization: `Bearer ${token}` },
+      }
+    );
+    const data = await response.json();
+    dispatch(setPosts({ posts: data }));
+  };
 
+  useEffect(() => {
+    if (isProfile) {
+      getUserPosts();
+    } else {
+      getPosts();
+    }
+  }, []); //eslint-disable-line react-hooks/exhaustive-deps
+};
 
 export default PostsWidget;
