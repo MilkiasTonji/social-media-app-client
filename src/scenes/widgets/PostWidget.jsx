ac66c5cec0014ad910bd2d040ee34327ed8a2f07
abebe.kayimo@crowdbotics.com <tongisttonji@gmail.com>
2023-03-04 17:12:50 +0300

    Latest update on March4 2023b

--- src/scenes/widgets/PostWidget.jsx
+++ src/scenes/widgets/PostWidget.jsx
@@ -1,33 +1,56 @@
-import { useTheme } from "@mui/material"
-import { useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-
+import { useTheme } from "@mui/material";
+import Friend from "components/Friend";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPost } from "state";
 
 const PostWidget = ({
-    postId, 
-    postUserId,
-    name,
-    description,
-    location,
-    picturePath,
-    userPicturePath,
-    likes,
-    comments
+  postId,
+  postUserId,
+  name,
+  description,
+  location,
+  picturePath,
+  userPicturePath,
+  likes,
+  comments,
 }) => {
-    const [isComments, setIsComments] = useState(false)    
-    const dispatch = useDispatch()
-    const token = useSelector((state)=> state.token)
-    const loggedInUserId = useSelector((state)=> state.user._id)
-    const isLiked = Boolean(likes[loggedInUserId])
-    const {palette} = useTheme()
-    const primaryLight = palette.primary.light
-    const primaryDark  = palette.primary.dark
-    const medium = palette.neutral.medium
+  const [isComments, setIsComments] = useState(false);
+  const dispatch = useDispatch();
+  const token = useSelector((state) => state.token);
+  const loggedInUserId = useSelector((state) => state.user._id);
+  const isLiked = Boolean(likes[loggedInUserId]);
+  const likeCount = Object.keys(likes).length;
 
-    
+  const { palette } = useTheme();
+  const primary = palette.primary.main;
+  const main = palette.primary.main;
 
-}
+  const patchLike = async () => {
+    const response = await fetch(`http://localhost:3001/posts/${postId}/like`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userId: loggedInUserId }),
+    });
 
+    const updatedPost = await response.json();
+    dispatch(setPost({ post: updatedPost }));
+  };
 
+  return (
+    <WidgetWrapper>
+      <Friend
+        friendId={postUserId}
+        name={name}
+        subtitle={location}
+        userPictruePath={userPicturePath}
+      />
+    </WidgetWrapper>
+  );
+};
 
 export default PostWidget;
