bd7b4ddbf48342fc77d77f078b27a8a1d77f0824
abebe.kayimo@crowdbotics.com <tongisttonji@gmail.com>
2023-03-07 23:33:15 +0300

    Latest update on March7 2023

--- src/scenes/widgets/AdvertWidget.jsx
+++ src/scenes/widgets/AdvertWidget.jsx
@@ -1,4 +1,5 @@
-import { useTheme } from "@mui/material";
+import { Typography, useTheme } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
 import WidgetWrapper from "components/WidgetWrapper";
 
 
@@ -13,7 +14,25 @@ const AdvertWidget = () => {
 
     return (
         <WidgetWrapper>
-            
+            <FlexBetween>
+                <Typography color={dark} variant="h5" fontWeight="500">
+                    Sponsored
+                </Typography>
+                <Typography color={medium}>
+                    Create Ad
+                </Typography>
+            </FlexBetween>
+            <img
+                width="100%"
+                height="auto"
+                alt="advert"
+                src="http://localhost:3001/assets/info4.jpeg"
+                style={{borderRadius: "0.75rem", margin: "0.75rem 0" }}
+            />
+            <FlexBetween>
+                <Typography color={main}>MikaCosmotics</Typography>
+                <Typography color={medium}>mikacosmotics.com</Typography>
+            </FlexBetween>
         </WidgetWrapper>
     )
 }
