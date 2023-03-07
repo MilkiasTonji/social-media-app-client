fc598fefb39bc945b0206e3b42d54a6c5f981efa
abebe.kayimo@crowdbotics.com <tongisttonji@gmail.com>
2023-03-07 23:54:01 +0300

    Latest update on March7 2023

--- src/scenes/widgets/FriendListWidget.jsx
+++ src/scenes/widgets/FriendListWidget.jsx
@@ -1,8 +1,9 @@
-import { useTheme } from "@mui/material"
+import { Box, Typography, useTheme } from "@mui/material"
+import WidgetWrapper from "components/WidgetWrapper"
+import Friend from 'components/Friend'
+import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { setFriends } from "state"
-
-const { default: WidgetWrapper } = require("components/WidgetWrapper")
 
 
 
@@ -26,9 +27,32 @@ const FriendListWidget = ({userId}) => {
         dispatch(setFriends({friends: data}))
     }
     
+    useEffect(() => {
+        getFriends();
+      }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+      
     return (
         <WidgetWrapper>
-            
+            <Typography color={palette.neutral.dark}
+            variant="h5" fontWeight="500"
+            sx={{mb: "1.5rem"}}
+            >
+                Friend List
+            </Typography>
+            <Box display="flex" flexDirection="column" gap="1.5rem">
+                {
+                    friends.map((friend)=> (
+                        <Friend
+                          key={friend._id}
+                          friendId={friend._id}
+                          name={`${friend.firstName} ${friend.lastName}`}
+                          subtitle={friend.occupation}
+                          userPicturePath={friend.picturePath}
+                        />
+                    ))
+                }
+            </Box>
         </WidgetWrapper>
     )
 }
