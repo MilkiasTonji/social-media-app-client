7cd14d1c7511aa40a0102706abd3b321673a0933
abebe.kayimo@crowdbotics.com <tongisttonji@gmail.com>
2023-03-07 23:44:49 +0300

    Latest update on March7 2023

--- src/scenes/widgets/FriendListWidget.jsx
+++ src/scenes/widgets/FriendListWidget.jsx
@@ -1,9 +1,31 @@
+import { useTheme } from "@mui/material"
+import { useDispatch, useSelector } from "react-redux"
+import { setFriends } from "state"
+
 const { default: WidgetWrapper } = require("components/WidgetWrapper")
 
 
 
 
-const FriendListWidget = () => {
+const FriendListWidget = ({userId}) => {
+
+    const dispatch = useDispatch()
+    const token = useSelector((state)=> state.token)
+    const friends = useSelector((state)=> state.user.friends)
+    const {palette} = useTheme()
+    
+    const getFriends = async ()=> {
+        const response = await fetch(`http://localhost:3001/users/${userId}/friends`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+
+        const data = await response.json();
+        dispatch(setFriends({friends: data}))
+    }
+    
     return (
         <WidgetWrapper>
             
